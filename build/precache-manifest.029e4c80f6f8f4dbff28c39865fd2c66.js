45ff0ede7f336c1611fe034c7bb75b003d7fcbe7
noirviper <christopher.r.johnson2@gmail.com>
2020-01-30 20:15:24 -0800

    adding build folder

---
+++ build/precache-manifest.029e4c80f6f8f4dbff28c39865fd2c66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9606c1efefcd14124152f8077d2334eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae5f56bd000ea3ac8588",
+    "url": "/static/css/main.36788609.chunk.css"
+  },
+  {
+    "revision": "7d277ae1d6169e9a3d7a",
+    "url": "/static/js/2.29a45c4b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.29a45c4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ae5f56bd000ea3ac8588",
+    "url": "/static/js/main.1898dbc9.chunk.js"
+  },
+  {
+    "revision": "0e3bd82152c247844649",
+    "url": "/static/js/runtime-main.d4873ae0.js"
+  }
+]);
